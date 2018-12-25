713dacef20100b9cf888b843e2240e8e88f22a62
Vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-25 09:09:09 -0800

    Initial espanol commit

---
+++ Upload/inc/languages/espanol/admin/guestnewsbars.lang.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * MyBB: Guest Newsbars
+ *
+ * File: guestnewsbars.lang.php
+ * 
+ * Authors: Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.0
+ * 
+ */
+
+// plugin_info
+
+$l['guestnewsbars_name'] = 'Barras de noticias invitadas';
+$l['guestnewsbars_desc'] = 'Agrega barras de noticias de invitados a la plantilla header_welcomeblock_guest de su foro para la visibilidad de los invitados.';
+$l['guestnewsbars_web'] = 'http://community.mybb.com/guest-6029.html';
+$l['guestnewsbars_auth'] = 'Vintagedaddyo';
+$l['guestnewsbars_authsite'] = 'http://community.mybb.com/guest-6029.html';
+$l['guestnewsbars_ver'] = '1.0';
+$l['guestnewsbars_compat'] = '18*';
+
+// Grupo de configuracion
+
+$l['guestnewsbars_title_setting_group'] = 'Barras de noticias invitadas';
+$l['guestnewsbars_description_setting_group'] = 'Configuracion para el complemento Barras de noticias invitadas.';
+
+// Configuracion 1
+
+$l['guestnewsbars_title_setting_1'] = '¿Desea habilitar el complemento de Guest Newsbars globalmente?';
+$l['guestnewsbars_description_setting_1'] = 'Si configura esta opcion en si, este complemento estara activo a nivel mundial.';
+
+// Configuracion 2
+
+$l['guestnewsbars_title_setting_2'] = 'Alert Display';
+$l['guestnewsbars_description_setting_2'] = 'Ingrese el texto que desea mostrar en Alert.';
+
+// Configuracion 3
+
+$l['guestnewsbars_title_setting_3'] = 'CSS para Guest Alert';
+$l['guestnewsbars_description_setting_3'] = 'Esto controla el estilo para la alerta de invitado.';
+
+// Configuracion 4
+
+$l['guestnewsbars_title_setting_4'] = '¿Desea habilitar la alerta de Barras de noticias invitadas?';
+$l['guestnewsbars_description_setting_4'] = 'Si configura esta opcion en si, la alerta estara activa en su tablero.';
+
+// Configuracion 5
+
+$l['guestnewsbars_title_setting_5'] = 'Display de aviso 1';
+$l['guestnewsbars_description_setting_5'] = 'Ingrese el texto que desea mostrar en el Aviso 1.';
+
+// Configuracion 6
+
+$l['guestnewsbars_title_setting_6'] = 'CSS para el Aviso de invitado 1';
+$l['guestnewsbars_description_setting_6'] = 'Esto controla el estilo del aviso de invitado1.';
+
+// Configuracion 7
+
+$l['guestnewsbars_title_setting_7'] = '¿Desea habilitar Barras de noticias invitadas Notice 1?';
+$l['guestnewsbars_description_setting_7'] = 'Si configura esta opcion en si, el Aviso 1 estara activo en su tablero.';
+
+// Configuracion 8
+
+$l['guestnewsbars_title_setting_8'] = 'Display 2 Display';
+$l['guestnewsbars_description_setting_8'] = 'Ingrese el texto que desea mostrar en el Aviso de invitado 2.';
+
+// Configuracion 9
+
+$l['guestnewsbars_title_setting_9'] = 'CSS para el Aviso de invitado 2';
+$l['guestnewsbars_description_setting_9'] = 'Esto controla el estilo del aviso de invitado2.';
+
+// Configuracion 10
+
+$l['guestnewsbars_title_setting_10'] = '¿Desea habilitar el Aviso 2 de Barras de noticias invitadas?';
+$l['guestnewsbars_description_setting_10'] = 'Si configura esta opcion en si, el Aviso 2 estara activo en su tablero.';
+
+?> 
