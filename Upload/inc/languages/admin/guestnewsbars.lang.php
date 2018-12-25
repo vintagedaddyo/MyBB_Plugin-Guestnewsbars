4931fca9be9beca04cf727806a6adb14f2a572f0
Vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-25 09:01:18 -0800

    Initial french commit

---
+++ Upload/inc/languages/admin/guestnewsbars.lang.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * MyBB: Guest Newsbars
+ *
+ * File: guestnewsbars.lang.php
+ * 
+ * Authors: Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.0
+ * 
+ */
+
+// plugin_info
+
+$l['guestnewsbars_name'] = 'Barre de presse invite';
+$l['guestnewsbars_desc'] = 'Ajoute des barres de news invite au modele header_welcomeblock_guest de votre forum pour la visibilite des invites.';
+$l['guestnewsbars_web'] = 'http://community.mybb.com/guest-6029.html';
+$l['guestnewsbars_auth'] = 'Vintagedaddyo';
+$l['guestnewsbars_authsite'] = 'http://community.mybb.com/guest-6029.html';
+$l['guestnewsbars_ver'] = '1.0';
+$l['guestnewsbars_compat'] = '18*';
+
+// Groupe de reglage
+
+$l['guestnewsbars_title_setting_group'] = 'Barre de presse invite';
+$l['guestnewsbars_description_setting_group'] = 'Parametres du plug-in Guest Newsbars.';
+
+// Reglage 1
+
+$l['guestnewsbars_title_setting_1'] = 'Voulez-vous activer le plug-in de barres de presse invitees globalement?';
+$l['guestnewsbars_description_setting_1'] = 'Si vous definissez cette option sur oui, ce plugin sera actif globalement.';
+
+// reglage 2
+
+$l['guestnewsbars_title_setting_2'] = 'Affichage des alertes';
+$l['guestnewsbars_description_setting_2'] = 'Entrez le texte que vous souhaitez afficher dans Alert.';
+
+// reglage 3
+
+$l['guestnewsbars_title_setting_3'] = 'CSS pour l alerte invite';
+$l['guestnewsbars_description_setting_3'] = 'Ceci controle le style de l alerte invite.';
+
+// reglage 4
+
+$l['guestnewsbars_title_setting_4'] = 'Souhaitez-vous activer l alerte sur les kiosques invites?';
+$l['guestnewsbars_description_setting_4'] = 'Si vous definissez cette option sur oui, Alert sera actif sur votre forum.';
+
+// reglage 5
+
+$l['guestnewsbars_title_setting_5'] = 'Avis 1 affichage';
+$l['guestnewsbars_description_setting_5'] = 'Entrez le texte que vous souhaitez afficher dans la notification 1.';
+
+// reglage 6
+
+$l['guestnewsbars_title_setting_6'] = 'CSS pour l avis d invite 1';
+$l['guestnewsbars_description_setting_6'] = 'Ceci controle le style pour guest notice1.';
+
+// reglage 7
+
+$l['guestnewsbars_title_setting_7'] = 'Souhaitez-vous activer la notification 1 des barres de presse invitees?';
+$l['guestnewsbars_description_setting_7'] = 'Si vous definissez cette option sur oui, la notification 1 sera active sur votre forum.';
+
+// reglage 8
+
+$l['guestnewsbars_title_setting_8'] = 'Avis 2 affichage';
+$l['guestnewsbars_description_setting_8'] = 'Entrez le texte que vous souhaitez afficher dans la notification d invite 2.';
+
+// Reglage 9
+
+$l['guestnewsbars_title_setting_9'] = 'CSS pour l avis d invite 2';
+$l['guestnewsbars_description_setting_9'] = 'Ceci contrôle le style de la notification de l invite2.';
+
+// Reglage 10
+
+$l['guestnewsbars_title_setting_10'] = 'Voulez-vous activer la notification 2 des barres de presse invite?';
+$l['guestnewsbars_description_setting_10'] = 'Si vous definissez cette option sur oui, la notification 2 sera active sur votre forum.';
+
+?> 
