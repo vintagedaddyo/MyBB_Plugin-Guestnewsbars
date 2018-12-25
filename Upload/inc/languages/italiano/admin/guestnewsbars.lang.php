c67bddf4bcca9ca4bf53fd98a9c5e3619587f4d4
Vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-25 08:53:41 -0800

    initial italiano commit

---
+++ Upload/inc/languages/italiano/admin/guestnewsbars.lang.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * MyBB: Guest Newsbars
+ *
+ * File: guestnewsbars.lang.php
+ * 
+ * Authors: Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.0
+ * 
+ */
+
+// plugin_info
+
+$l['guestnewsbars_name'] = 'Newsbars ospiti';
+$l['guestnewsbars_desc'] = 'Aggiunge le newsbar degli ospiti al modello header_welcomeblock_guest del forum per la visibilita degli ospiti.';
+$l['guestnewsbars_web'] = 'http://community.mybb.com/guest-6029.html';
+$l['guestnewsbars_auth'] = 'Vintagedaddyo';
+$l['guestnewsbars_authsite'] = 'http://community.mybb.com/guest-6029.html';
+$l['guestnewsbars_ver'] = '1.0';
+$l['guestnewsbars_compat'] = '18*';
+
+// Impostazione del gruppo
+
+$l['guestnewsbars_title_setting_group'] = 'Newsbars ospiti';
+$l['guestnewsbars_description_setting_group'] = 'Impostazioni per il plugin Newsbars ospiti.';
+
+// Impostazione 1
+
+$l['guestnewsbars_title_setting_1'] = 'Vuoi abilitare il plugin per le newsbars ospiti a livello globale?';
+$l['guestnewsbars_description_setting_1'] = 'Se si imposta questa opzione su yes, questo plugin sara attivo globalmente.';
+
+// Impostazione 2
+
+$l['guestnewsbars_title_setting_2'] = 'Alert Display';
+$l['guestnewsbars_description_setting_2'] = 'Inserisci il testo che vuoi visualizzare in Alert.';
+
+// Impostazione 3
+
+$l['guestnewsbars_title_setting_3'] = 'CSS per avviso ospite';
+$l['guestnewsbars_description_setting_3'] = 'Controlla lo stile per gli avvisi degli ospiti.';
+
+// Impostazione 4
+
+$l['guestnewsbars_title_setting_4'] = 'Vuoi abilitare l avviso delle newsbars ospiti?';
+$l['guestnewsbars_description_setting_4'] = 'Se imposti questa opzione su yes, Alert sara attivo sulla tua bacheca.';
+
+// Impostazione 5
+
+$l['guestnewsbars_title_setting_5'] = 'Avviso 1 Visualizza';
+$l['guestnewsbars_description_setting_5'] = 'Inserisci il testo che vuoi visualizzare nell Avviso 1.';
+
+// Impostazione 6
+
+$l['guestnewsbars_title_setting_6'] = 'CSS for Ospiti Notice 1';
+$l['guestnewsbars_description_setting_6'] = 'Controlla lo stile per il ospiti notice1.';
+
+// Impostazione 7
+
+$l['guestnewsbars_title_setting_7'] = 'Vuoi abilitare la notifica 1 di Newsbars Ospiti?';
+$l['guestnewsbars_description_setting_7'] = 'Se si imposta questa opzione su yes, l avviso 1 sara attivo sulla propria scheda.';
+
+// Impostazione 8
+
+$l['guestnewsbars_title_setting_8'] = 'Notice 2 Display';
+$l['guestnewsbars_description_setting_8'] = 'Inserisci il testo che vuoi visualizzare in Ospiti Notice 2.';
+
+// Impostazione 9
+
+$l['guestnewsbars_title_setting_9'] = 'CSS per Ospiti Notice 2';
+$l['guestnewsbars_description_setting_9'] = 'Controlla lo stile per il ospiti notice2.';
+
+// Impostazione 10
+
+$l['guestnewsbars_title_setting_10'] = 'Vuoi abilitare la notifica 2 di Newsbars Ospiti?';
+$l['guestnewsbars_description_setting_10'] = 'Se si imposta questa opzione su yes, l avviso 2 sara attivo sulla propria scheda.';
+
+?> 
